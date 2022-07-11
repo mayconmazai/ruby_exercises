a637780a1c34588319b6a6857721bdbae9eebd49
mayconmazai <mayconmazai@gmail.com>
2022-07-11 14:56:45 -0300

    Working with each

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -1,6 +1,6 @@
 def cipher(string, factor)
     code = []
-    for letter in string.bytes
+    string.bytes.each do |letter|
         if letter + factor > 122 || letter + factor > 90 && letter + factor < 96
             code << letter + factor - 26
         elsif letter.between?(65, 90) || letter.between?(97, 122)
@@ -9,6 +9,6 @@ def cipher(string, factor)
             code << letter
         end
     end
-    puts code.pack('C*')
+    return code.pack('C*')
 end
-cipher("What a string!", 5) # => "Bmfy f xywnsl!")
+puts cipher("What a string!", 5) # => "Bmfy f xywnsl!")
