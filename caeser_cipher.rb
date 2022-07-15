307c4a5c1977d800590c18d692cac006e596f757
mayconmazai <mayconmazai@gmail.com>
2022-07-15 08:41:18 -0300

    four ways to cipher

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -1,3 +1,49 @@
+def simple_short_cipher(string, factor)
+    alphabet = ('a'..'z').to_a
+    code = []
+    string.each_char do |char|
+        if alphabet.index(char)
+            code << alphabet[(alphabet.index(char) + factor) % alphabet.length]
+        elsif alphabet.index(char.downcase) && char.upcase
+            code << alphabet[(alphabet.index(char.downcase) + factor) % alphabet.length].upcase
+        else
+            code << char
+        end
+    end
+    return code.join
+end
+puts simple_short_cipher("What a string!", 5) # => "Bmfy f xywnsl!"
+
+def simple_cipher(string, factor)
+    lower_case = ('a'..'z').to_a
+    upper_case = ('A'..'Z').to_a
+    code = []
+    string.each_char do |char|
+        if lower_case.include?(char) 
+            result = lower_case.index(char) + factor
+            if result > lower_case.length
+                result = result - lower_case.length
+                code << lower_case[result]
+            else
+                code << lower_case[result]
+            end
+            
+        elsif upper_case.include?(char)
+            result = upper_case.index(char) + factor
+            if result > upper_case.length
+                result = result - upper_case.length
+                code << upper_case[result]
+            else
+                code << upper_case[result]
+            end
+        else
+            code << char
+        end
+    end
+    return code.join
+end
+puts simple_cipher("What a string!", 5) # => "Bmfy f xywnsl!"
+
 def cipher(string, factor)
     code = []
     string.bytes.each do |letter|
@@ -11,4 +57,13 @@ def cipher(string, factor)
     end
     return code.pack('C*')
 end
-puts cipher("What a string!", 5) # => "Bmfy f xywnsl!")
+puts cipher("What a string!", 5) # => "Bmfy f xywnsl!"
+
+def caeser_cipher(string, factor)
+    code = []
+    string.bytes.each do |letter|
+            code << letter + factor 
+    end
+    return code.pack('C*')
+end
+puts caeser_cipher("What a string!", 5) # => "\mfy%f%xywnsl&"
