15a1756853dfc95802fdd5db658e5fdcdb6d76fa
mayconmazai <mayconmazai@gmail.com>
2022-08-01 08:41:23 -0300

    Stock Picker working

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,17 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-def substrings(string, dictionary)
-  dictionary.each do |word|
-    string.include?(word)
-    puts word + " = " + string
-  end
+price_by_day = [17,3,6,9,15,8,6,1,10] # => Find the best prices betwen day 0 and 8.
+
+def stock_picker(price_by_day)
+  best_gain = 0
+  best_days = []
+    price_by_day.each_with_index do |price, day|
+        price_by_day.each_with_index do |price2, day2|     
+            if day2 > day && price2 - price > best_gain
+                best_gain = price2 - price
+                best_days = [day, day2]
+            end
+        end
+    end
+    puts "Best days to buy: #{best_days[0]}, sell: #{best_days[1]}"    
 end
-substrings("below", dictionary)
+
+stock_picker(price_by_day)
